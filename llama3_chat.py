c84d66c277fdb7a60804b7aadeb57c195cfc2554
atabahr <atabahar09@gmail.com>
2024-08-09 18:07:06 -0700

    Update_2
    2nd Update

--- llama3_chat.py
+++ llama3_chat.py
@@ -9,8 +9,6 @@ import os
 
 st.subheader("Llama 3.1 405B Chatbot")
 
-st.markdown("Built by [Build Fast with AI](https://www.buildfastwithai.com/genai-course)")
-
 with st.sidebar:
     st.title("Llama 3.1 405B Chatbot")
     st.subheader("This app lets you chat with Llama 3.1 405B! [👉]")
